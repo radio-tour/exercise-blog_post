82306e865a96e40e9fa9915884683e7a6a4957e8
insorker <2677331029@qq.com>
2024-04-17 20:37:59 +0800

    feature: update

--- src/lib.rs
+++ src/lib.rs
@@ -1,81 +1,43 @@
 pub struct Post {
-  state: Option<Box<dyn State>>,
   content: String,
 }
 
 impl Post {
-  pub fn new(&self) -> Self {
-    Post {
-      state: Some(Box::new(Draft { })),
-      content: String::from(""),
+  pub fn new() -> DraftPost {
+    DraftPost {
+      content: String::new(),
     }
   }
 
   pub fn content(&self) -> &str {
-    self.state.as_ref().unwrap().content(self)
+    &self.content
   }
+}
 
+pub struct DraftPost {
+  content: String,
+}
+
+impl DraftPost {
   pub fn add_text(&mut self, text: &str) {
     self.content.push_str(text);
   }
 
-  pub fn request_review(&mut self) {
-    if let Some(s) = self.state.take() {
-      self.state = Some(s.request_review());
-    }
-  }
-
-  pub fn approve(&mut self) {
-    if let Some(s) = self.state.take() {
-      self.state = Some(s.approve());
+  pub fn request_review(self) -> PendingReivewPost {
+    PendingReivewPost {
+      content: self.content
     }
   }
 }
 
-pub trait State {
-  fn request_review(self: Box<Self>) -> Box<dyn State>;
-  fn approve(self: Box<Self>) -> Box<dyn State>;
-  fn content<'a>(&self, _: &'a Post) -> &'a str {
-    ""
-  }
+pub struct PendingReivewPost {
+  content: String,
 }
 
-pub struct Draft { }
-
-impl State for Draft {
-  fn request_review(self: Box<Self>) -> Box<dyn State> {
-    Box::new(PendingReview { })
-  }
-
-  fn approve(self: Box<Self>) -> Box<dyn State> {
-    self
-  }
-}
-
-pub struct PendingReview { }
-
-impl State for PendingReview {
-  fn request_review(self: Box<Self>) -> Box<dyn State> {
-    self
-  }
-
-  fn approve(self: Box<Self>) -> Box<dyn State> {
-    Box::new(Published { })
-  }
-}
-
-pub struct Published { }
-
-impl State for Published {
-  fn request_review(self: Box<Self>) -> Box<dyn State> {
-    self
-  }
-
-  fn approve(self: Box<Self>) -> Box<dyn State> {
-    self
-  }
-
-  fn content<'a>(&self, post: &'a Post) -> &'a str {
-    &post.content
+impl PendingReivewPost {
+  pub fn approve(self) -> Post {
+    Post {
+      content: self.content
+    }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use exercise_blog_post::*;
+
 fn main() {
-  println!("Hello, world!");
+  let mut post = Post::new();
+
+  post.add_text("Hello World!");
+
+  let post = post.request_review();
+
+  let post = post.approve();
+
+  assert_eq!("Hello World!", post.content());
 }
